4062845559539a6da2b49407b215433aa6a72f8c
pobregon147@gmail.com <pobregon147@gmail.com>
2023-02-18 13:48:20 -0500

    5to commit

--- registro.php
+++ registro.php
@@ -4,19 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CARGO DE NOTIFICACIONES</title>
     <link rel="stylesheet" href="styles.css">
     
 </head>
 <body>
-<head>
-        <title>Formulario de registro</title>
-    </head>
-    <body>
-    <button id="btn-modal">Registrar usuario</button>
-    <div id="modal" class="modal">
-  <div class="modal-content">
-  <form method="post" action="procesar_registro.php">
+
+<div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="miModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="miModalLabel">Registro de cargos</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+      <form method="post" action="procesar_registro.php">
             <label for="nombre">Nombre:</label>
             <input type="text" id="nombre" name="nombre" required><br>
             
@@ -38,19 +43,32 @@
                 <option value="femenino">Femenino</option>
                 <option value="otro">Otro</option>
             </select><br>
-            
             <input type="submit" value="Registrar">
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Guardar cambios</button>
+      </div>
+    </div>
   </div>
 </div>
-        
 
-            <?php
-if (isset($_GET['mensaje'])) {
-    echo "<p>" . $_GET['mensaje'] . "</p>";
-}
-?>
-        </form>
-        <script src="modal.js"></script>
+<?php if (isset($_GET['mensaje'])) {echo "<p>" . $_GET['mensaje'] . "</p>";}?>
+<!-- Incluir jQuery y Bootstrap.js antes de cerrar el body -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+<!-- Incluir tu código JavaScript para mostrar el modal -->
+<script>
+  $(document).ready(function(){
+    $('#miBoton').click(function(){
+      $('#miModal').modal('show');
+    });
+  });
+</script>
+
 </body>
 </html>
 
