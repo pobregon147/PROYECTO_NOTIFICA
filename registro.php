c1a6a33b621012a8c963eb388a4a45ef8487a785
pobregon147@gmail.com <pobregon147@gmail.com>
2023-02-19 18:03:20 -0500

    18 commit

--- registro.php
+++ registro.php
@@ -74,7 +74,7 @@
   </div>
 </div>
 
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modaldatos">Registrar datos</button>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="modaldatos">Registrar datos</button>
 
 <div class="modal fade" id="modaldatos" tabindex="-1" role="dialog" aria-labelledby="miModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
@@ -103,9 +103,9 @@
               <span class="input-group-text">Usuario:</span>
               </div>
               <input type="text" id="nombre" name="nombre" class="form-control" disabled required >
-              <?php echo $fila['nombres']; ?>
+              <?php echo $fila['nombre']; ?>
               <input type="text" id="apellido" name="apellido" class="form-control" disabled required ><br>
-              <?php echo $fila['apellidos']; ?>
+              <?php echo $fila['apellido']; ?>
             </div>
             <?php endwhile; ?>
       </div>
@@ -140,6 +140,7 @@
     </div>
   </div>
 </div>
+
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
