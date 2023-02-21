2815dc74c6e49427e1baf01cab207fdbe8c1a702
Pedro Antonio Obregon Coronado <pedro.obregon@UGEL02.GOB.PE>
2023-02-21 13:50:32 -0500

    corregido

--- registro.php
+++ registro.php
@@ -170,10 +170,6 @@ try {
             <th>GENERO</th>
             <th>EMAIL</th>
             <th>TELEFONO</th>
-<<<<<<< HEAD
-=======
-            <th>EMAIL</th>
->>>>>>> e751686410bb73a79418c66678eb20b8e72b32cc
             <th>COMENTARIO</th>
         </tr>
     </thead>
