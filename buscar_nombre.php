9a2089de1c31a7040120b0d2ca19441ba8082d82
pobregon147@gmail.com <pobregon147@gmail.com>
2023-02-20 20:40:36 -0500

    26 commit

--- buscar_nombre.php
+++ buscar_nombre.php
@@ -12,26 +12,19 @@ try {
     echo "Error al conectar a la base de datos: " . $e->getMessage();
 }
 
-try {
-    $conn = new PDO("sqlsrv:server=$serverName;database=$database", $username, $password);
-    $statement = $conexion->prepare("SELECT nombre, apellido FROM registros WHERE id = :id");
-    $statement->bindParam(":id", $id);
-    $statement->execute();
-    $fila = $statement->fetch(PDO::FETCH_ASSOC);
-  
-    if ($fila) {
-      $nombre = $fila["nombre"];
-      $apellido = $fila["apellido"];
-      $respuesta = array("nombre" => $nombre, "apellido" => $apellido);
-      echo json_encode($respuesta);
-    } else {
-      $respuesta = array("nombre" => "", "apellido" => "");
-      echo json_encode($respuesta);
-    }
-  } catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-  }
-  
+// Obtener el ID del usuario enviado desde el formulario
+$id = $_POST["id"];
+
+// Consultar la base de datos para obtener el nombre y apellido del usuario con el ID especificado
+$query = "SELECT nombre, apellido FROM usuarios WHERE id = :id";
+$statement = $conn->prepare($query);
+$statement->bindParam(':id', $id);
+$statement->execute();
+$datos_usuario = $statement->fetch(PDO::FETCH_ASSOC);
+
+// Devolver los datos del usuario en formato JSON
+echo json_encode($datos_usuario);
+
 // Cerrar la conexión a la base de datos
-$conn = null;
+$conexion = null;
 ?>
