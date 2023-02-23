d66e35a5a1c38ae2d9fdb59f9f8c095006d2cb03
Pedro Antonio Obregon Coronado <POBREGON147@GMAIL.com>
2023-02-23 10:29:02 -0500

    otr avez aqui

--- buscar.php
+++ buscar.php
@@ -1,5 +1,16 @@
 <?php
-// Conectarse a la base de datos
+$serverName = "servidornotificaciones.database.windows.net";
+$database = "bdnotificaciones";
+$username = "administradorsql";
+$password = "5720805Po";
+
+// Conexión a la base de datos utilizando PDO
+try {
+    $conn = new PDO("sqlsrv:server=$serverName;database=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error al conectar a la base de datos: " . $e->getMessage();
+}
 
 if (isset($_POST['searchValue'])) {
   $searchValue = $_POST['searchValue'];
