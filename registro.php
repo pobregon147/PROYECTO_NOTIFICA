f1fba7283cc2b4dcfe9323acc5dfc37d0a659931
Pedro Antonio Obregon Coronado <POBREGON147@GMAIL.com>
2023-03-02 14:58:19 -0500

    cambido20

--- registro.php
+++ registro.php
@@ -107,17 +107,7 @@ try {
 
       <form method="post" action="procesar_datos.php">
 
-      <div class="form-row">
-            <div class="form-group col-md-6">
-            <label for="id">ID:</label>
-            <input type="text" id="id" name="id" class="form-control" required>
-            </div>
-            <div class="form-group col-md-6">
-            <button type="button" class="btn btn-primary" id="buscar">Buscar</button>
-            </div><br>
-              <input type="text" id="nombre" name="nombre" class="form-control" readonly>
-              <input type="text" id="apellido" name="apellido" class="form-control" readonly><br>
-      </div>
+      
 
       <div class="form-row">
             <div class="form-group col-md-6">
@@ -186,7 +176,17 @@ try {
         <?php endforeach ?>
     </tbody>
 </table>
-
+<div class="form-row">
+            <div class="form-group col-md-6">
+            <label for="id">ID:</label>
+            <input type="text" id="id" name="id" class="form-control" required>
+            </div>
+            <div class="form-group col-md-6">
+            <button type="button" class="btn btn-primary" id="buscar">Buscar</button>
+            </div><br>
+              <input type="text" id="nombre" name="nombre" class="form-control" readonly>
+              <input type="text" id="apellido" name="apellido" class="form-control" readonly><br>
+      </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
