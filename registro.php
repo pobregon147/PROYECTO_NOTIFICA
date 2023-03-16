a68e8f2d462af92b9b3d49028359afbff6c397e9
pobregon147@gmail.com <pobregon147@gmail.com>
2023-03-15 19:59:09 -0500

    commit7

--- registro.php
+++ registro.php
@@ -241,11 +241,12 @@ $apellido = $result['NOMBRES'];
     </tbody>
 </table>
 
+<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/v/bs4/dt-1.13.4/datatables.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script> 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdn.datatables.net/v/bs4/dt-1.13.4/datatables.min.js"></script>
 <script>
 // Obtener el elemento de entrada y la tabla
 var input = document.getElementById("searchInput");
@@ -271,9 +272,12 @@ input.addEventListener("keyup", function() {
   }
 });
 </script>
+
 <script>$(document).ready( function () {
     $('#msearchResults').DataTable();
-} );</script>
+} );
+</script>
+
 </body>
 </html>
 
