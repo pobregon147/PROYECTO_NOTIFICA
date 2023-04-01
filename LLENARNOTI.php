4d9e1af228fed73875596711d0827fe64f4884fb
pobregon147@gmail.com <pobregon147@gmail.com>
2023-04-01 00:15:43 -0500

    commit10

--- LLENARNOTI.php
+++ LLENARNOTI.php
@@ -1,28 +1,23 @@
 <?php
+// Conectar a la base de datos
+require 'conexion.php';
+
 // Obtener los datos de la notificación del formulario
 $direccion = $_POST['direccion'];
 $distrito = $_POST['distrito'];
 $notificador = 'PEDRO OBREGON'; // valor fijo
 $fecha_noti = date('Y-m-d'); // fecha actual
 $estado = 'NOTIFICADO'; // valor fijo
-
-// Conectar a la base de datos
-require_once('conexion.php');
-
-// Preparar la consulta SQL
-$sql = "INSERT INTO  registros SET DIRECCION = :direccion, DISTRITO = :distrito, NOTIFICADOR = :notificador, FECHA_NOTI = :fecha_noti, ESTADO = :estado WHERE N_CARGOS = :id";
-
-// Obtener el ID del registro a actualizar (pasado por el formulario)
-$id = $_POST['id'];
+$id = $_POST['N_CARGOS'];
 
 // Ejecutar la consulta SQL
-$stmt = $conn->prepare($sql);
+$stmt = $conn->prepare("UPDATE registros SET DIRECCION = :direccion, DISTRITO = :distrito, NOTIFICADOR = :notificador, FECHA_NOTI = :fecha_noti, ESTADO = :estado WHERE N_CARGOS = :N_CARGOS");
 $stmt->bindParam(':direccion', $direccion);
 $stmt->bindParam(':distrito', $distrito);
 $stmt->bindParam(':notificador', $notificador);
 $stmt->bindParam(':fecha_noti', $fecha_noti);
 $stmt->bindParam(':estado', $estado);
-$stmt->bindParam(':id', $id);
+$stmt->bindParam(':N_cARGOS', $id);
 $stmt->execute();
 
 // Redireccionar a la página anterior
